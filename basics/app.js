880527c6b692e7121f1fb9033e0f71cf69e062c6
Arthur Margarian <arthurmargarian1997@gmail.com>
2019-02-27 22:13:18 +0400

    first commit

---
+++ basics/app.js
@@ -0,0 +1,84 @@
+//--------------------------------------------------------- video 5 -----------------------------------------------
+// console.clear()
+// console.table({a:1,b:2});
+// console.time()
+//     console.log('test');
+//     console.log('test');
+//     console.log('test');
+//     console.log('test');
+// console.timeEnd()
+
+
+//--------------------------------------------------------- video 6 -----------------------------------------------
+// consts can be chacnged like array and objcet, but cant be reassigned
+
+
+//--------------------------------------------------------- video 7 -----------------------------------------------
+// type of null is object (mistake/bug)
+// all refernse types are objects (array, object, function, date etc.)
+
+
+//--------------------------------------------------------- video 8 -----------------------------------------------
+// lenght only for string, not for ints
+// typr of NaN is number
+// String(5) & (5).toString()  /  Number('5') & parseInt('5'), parseFloat('5')
+
+
+//--------------------------------------------------------- video 9 -----------------------------------------------
+// Math.abs(-2), Math.pow(8,2), Math.sqrt(64), Math.min(-2,1,55,8), Math.max(-2,1,55,8)
+
+
+//--------------------------------------------------------- video 10 ----------------------------------------------
+// var str = 'Hello';
+// var val = str[1];
+// var val = str.indexOf('e');
+// var val = str.charAt('1');
+// var val = str.includes('ll');
+// console.log(val);
+
+
+//--------------------------------------------------------- video 11 ----------------------------------------------
+// template strings `` (es6)
+// var name = 'Test'
+// var age = 12
+// function myAge() {
+//     return 12;
+// }
+// var html = `Hello ${name}, age ${6+6} or ${myAge()} and ${age > 10 ? 'Over' : 'Under'}`
+// console.log(html);
+
+
+//--------------------------------------------------------- video 14 ----------------------------------------------
+// var today  = new Date()
+// var val = today.getTime()
+// console.log(val); //seconds passed since 01-01-1970
+
+
+//--------------------------------------------------------- video 17 ----------------------------------------------//
+// // function declarations
+// function hello(name) {
+//     return `hello ${name}`;
+// }
+// console.log(hello('Test'));
+
+// // function expresion
+// var myhello = function(name){
+//     return `hello ${name}`;
+// }
+// console.log(myhello('Test'));
+
+// // functions invokable
+// (function(name){
+//     console.log(`hello ${name}`);
+// })('Test');
+
+// // property methods
+// const helloObj = {
+//     do: function(name){
+//         return `hello ${name}`;
+//     }
+// }
+// console.log(helloObj.do('Test'));
+
+
+//--------------------------------------------------------- video 19 ----------------------------------------------
