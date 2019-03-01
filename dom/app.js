9b10d7440d654f0e1b2608fe9aabcb81e08a26be
Arthur Margarian <arthurmargarian1997@gmail.com>
2019-03-01 21:44:24 +0400

    DOM

---
+++ dom/app.js
@@ -0,0 +1,37 @@
+//--------------------------------------------------------- video 22 -----------------------------------------------
+// let scripts = document.scripts;
+// let scriptsArr = Array.from(scripts); //array from collection
+// scriptsArr.forEach(function(script) {
+//   console.log(script.getAttribute('src'));
+// });
+
+
+//--------------------------------------------------------- video 23 -----------------------------------------------
+// console.log(document.querySelector('h5')); // will get the first one
+
+
+//--------------------------------------------------------- video 24 -----------------------------------------------
+// HTMLCollection like an array, but need to be converted, no need for NodeList
+//foreach will work only for NodeList, for with both (HTMLCollection/NodeList)
+
+
+//--------------------------------------------------------- video 25 -----------------------------------------------
+// document.querySelector('ul.collection').childNodes[3].nodeType;
+//all nodetypes
+// 1 - Element
+// 2 - Attribute (deprecated)
+// 3 - Text node
+// 8 - Comment
+// 9 - Document itself
+// 10 - Doctype
+// val = listItem.parentNode; //Nodelist item
+// val = listItem.parentElement; //DOM element
+
+
+//--------------------------------------------------------- video 27 -----------------------------------------------//
+// Replace element.replaceChild(newHeading, oldHeading);
+
+
+//--------------------------------------------------------- video 32 -----------------------------------------------
+// localStorage.setItem('name', 'John'); //will stay until manually delete
+// sessionStorage.setItem('name', 'Beth'); //will gone with beowser closing
